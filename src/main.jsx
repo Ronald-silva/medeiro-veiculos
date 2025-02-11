583456aa4a8c16683637ea4e322689ad01a17ca2
Ronald-silva <ronaldsilvajob@yahoo.com>
2025-02-11 10:23:25 -0300

    otimization

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const root = createRoot(document.getElementById('root'))
+const container = document.getElementById('root')
+if (!container) throw new Error('Elemento root não encontrado')
+
+const root = ReactDOM.createRoot(container)
 
 root.render(
   <React.StrictMode>
